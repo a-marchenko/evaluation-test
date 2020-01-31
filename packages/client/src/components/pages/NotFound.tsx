77bb79c25047831c5ecfb9a2a8315da7d40eb205
accuradev <dev.marchenko@gmail.com>
2020-01-31 21:41:58 +0300

    feat(client): add NotFound page

---
+++ packages/client/src/components/pages/NotFound.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+
+import CenteredLayout from '../layouts/CenteredLayout/CenteredLayout';
+import RedirectMessage from '../features/redirect/RedirectMessage/RedirectMessage';
+import DefaultButton from '../common/DefaultButton/DefaultButton';
+
+const NotFound = () => {
+  const history = useHistory();
+  const { pathname } = useLocation();
+
+  return (
+    <CenteredLayout>
+      <RedirectMessage title="Error 404 :(" text="No match found for:" path={pathname} />
+      <DefaultButton
+        type="button"
+        text="Go to homepage"
+        onClick={() => history.push('/')}
+        width={240}
+        isPrimary
+      />
+    </CenteredLayout>
+  );
+};
+
+export default NotFound;
