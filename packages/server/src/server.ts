1fa5a99c89e28ed702bef425f1293cfa9a07a0e3
accuradev <dev.marchenko@gmail.com>
2020-02-01 01:40:46 +0300

    refactor(server): reorganize server handleRoutes method

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,12 +1,11 @@
 import fs from 'fs';
 import path from 'path';
-import url from 'url';
 import { createServer, Server as HTTPSServer } from 'https';
 import express, { Application } from 'express';
 import socketIO, { Server as SocketIOServer } from 'socket.io';
 import cookieParser from 'cookie-parser';
 
-import router from './router';
+import router from './api/router';
 
 export class Server {
   private httpsServer: HTTPSServer;
@@ -43,71 +42,41 @@ export class Server {
   }
 
   private handleRoutes(): void {
-    // handles api requests
-    this.app.use('/api/', router);
+    // handle api routes
+    this.app.use('/api', router);
 
     // handle login route
-    this.app.use('/login', (req, res) => {
-      const URT: string = req.cookies.URT;
-      if (URT) {
-        res.redirect('/home');
-      } else {
-        res.sendFile(path.join(__dirname + '../../../client/dist/index.html'));
-      }
-    });
-
-    // handle login route
-    this.app.use('/register', (req, res) => {
-      const URT: string = req.cookies.URT;
-      if (URT) {
-        res.redirect('/home');
-      } else {
-        res.sendFile(path.join(__dirname + '../../../client/dist/index.html'));
-      }
-    });
-
-    // handles access route
-    this.app.get('/access', (req, res) => {
-      // try to get UAT or URT from header / cookie
-      const UAT: string | undefined = req.headers.authorization?.split(' ')[1];
+    this.app.get('/login', (req, res) => {
+      // try to get URT cookie
       const URT: string | undefined = req.cookies.URT;
 
-      if (URT && UAT) {
-        // if all tokens found redirect to home
-        res.redirect('/home');
-      } else if (!URT) {
-        // if no URT found redirect to login
-        res.redirect('/login');
+      if (URT) {
+        // try to refresh tokens if got URT
+        res.redirect(307, '/access');
       } else {
-        // if got URT send files
+        // serve client
+        res.sendFile(path.join(__dirname + '../../../client/dist/index.html'));
+      }
+    });
+
+    // handle register route
+    this.app.get('/register', (req, res) => {
+      // try to get UAT or URT from header / cookie
+      const URT: string | undefined = req.cookies.URT;
+
+      if (URT) {
+        // try to refresh tokens if got URT
+        res.redirect(307, '/access');
+      } else {
+        // serve client
         res.sendFile(path.join(__dirname + '../../../client/dist/index.html'));
       }
     });
 
     // handles any requests that don't match the ones above
-    this.app.get('*', (req, res) => {
-      // try to get UAT or URT from header / cookie
-      const UAT: string | undefined = req.headers.authorization?.split(' ')[1];
-      const URT: string | undefined = req.cookies.URT;
-
-      if (!URT && !UAT) {
-        // if no tokens found redirect to login
-        res.redirect('/login');
-      } else if (!UAT) {
-        // try to refresh tokens if no UAT found but got URT
-        res.redirect(
-          307,
-          url.format({
-            pathname: '/access',
-            query: {
-              target: req.path,
-            },
-          }),
-        );
-      } else {
-        // all ok send files
-        res.sendFile(path.join(__dirname + '../../../client/dist/index.html'));
-      }
+    this.app.get('*', (_, res) => {
+      // serve client
+      res.sendFile(path.join(__dirname + '../../../client/dist/index.html'));
     });
   }
 
