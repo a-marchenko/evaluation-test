db9cd634755daab444b6c1be346ab56bae40f9ae
accuradev <dev.marchenko@gmail.com>
2020-01-31 21:38:15 +0300

    feat(client): add new name and ref props to FormInput component

--- packages/client/src/components/common/FormInput/FormInput.tsx
+++ packages/client/src/components/common/FormInput/FormInput.tsx
@@ -3,20 +3,24 @@ import React from 'react';
 import styles from './FormInput.css';
 
 interface Props {
+  name: string;
   type: 'text' | 'email' | 'password';
   placeholder: string;
   error?: string;
+  register?: any;
 }
 
 const FormInput = (props: Props) => {
   return (
     <section className={styles.field}>
       <input
+        name={props.name}
         type={props.type}
         placeholder={props.placeholder}
         className={
           !props.error ? styles.field__input : styles.field__input + ' ' + styles.field__inputError
         }
+        ref={props.register}
       />
       {props.error && <span className={styles.field__error}>{props.error}</span>}
     </section>
