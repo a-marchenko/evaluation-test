da8bd46e583270a473166722d79ce1dfa6a19bb6
accuradev <dev.marchenko@gmail.com>
2020-02-01 01:45:42 +0300

    refactor(client): remove deprecated imports and logic

--- packages/client/src/components/pages/Login.tsx
+++ packages/client/src/components/pages/Login.tsx
@@ -1,22 +1,10 @@
-import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 
 import CenteredLayout from '../layouts/CenteredLayout/CenteredLayout';
 import EnteranceHeader from '../features/enterance/EnteranceHeader/EnteranceHeader';
 import LoginForm from '../features/enterance/LoginForm/LoginForm';
 
-import { useStore } from '../../state/store';
-
 const Login = () => {
-  const { state } = useStore();
-  const history = useHistory();
-
-  useEffect(() => {
-    if (state.accessToken) {
-      history.push('/home');
-    }
-  }, []);
-
   return (
     <CenteredLayout>
       <EnteranceHeader title="Login to account" text="Please, login to your account to use chat" />
--- packages/client/src/components/pages/Register.tsx
+++ packages/client/src/components/pages/Register.tsx
@@ -1,22 +1,10 @@
-import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 
 import CenteredLayout from '../layouts/CenteredLayout/CenteredLayout';
 import EnteranceHeader from '../features/enterance/EnteranceHeader/EnteranceHeader';
 import RegisterForm from '../features/enterance/RegisterForm/RegisterForm';
 
-import { useStore } from '../../state/store';
-
 const Register = () => {
-  const { state } = useStore();
-  const history = useHistory();
-
-  useEffect(() => {
-    if (state.accessToken) {
-      history.push('/home');
-    }
-  }, []);
-
   return (
     <CenteredLayout>
       <EnteranceHeader title="Register an account" text="Please, register an account to use chat" />
