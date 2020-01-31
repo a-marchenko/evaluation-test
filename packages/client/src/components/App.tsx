457724436c9a887feada7c7790703b79a269e740
accuradev <dev.marchenko@gmail.com>
2020-01-31 21:45:33 +0300

    feat(client): add new routes to the app and provide global store

--- packages/client/src/components/App.tsx
+++ packages/client/src/components/App.tsx
@@ -3,30 +3,46 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Access from './pages/Access';
+import NotFound from './pages/NotFound';
+import { StoreProvider } from '../state/store';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route
-          path="/"
-          render={({ location }) => (
-            <Redirect
-              to={{
-                pathname: '/login',
-                state: { from: location },
-              }}
-            />
-          )}
-        />
-      </Switch>
-    </BrowserRouter>
+    <StoreProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/access">
+            <Access />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route
+            path="/"
+            exact={true}
+            render={({ location }) => (
+              <Redirect
+                to={{
+                  pathname: '/home',
+                  state: { from: location },
+                }}
+              />
+            )}
+          />
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </StoreProvider>
   );
 };
 
