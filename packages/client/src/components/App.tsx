01d30a2a5744c4221d611dc14d40e770320d33d1
accuradev <dev.marchenko@gmail.com>
2020-02-01 01:43:04 +0300

    feat(client): use new router components

--- packages/client/src/components/App.tsx
+++ packages/client/src/components/App.tsx
@@ -1,47 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Access from './pages/Access';
-import NotFound from './pages/NotFound';
+import Router from './routes/Router';
+
 import { StoreProvider } from '../state/store';
-import Home from './pages/Home';
 
 const App = () => {
   return (
     <StoreProvider>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/access">
-            <Access />
-          </Route>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route
-            path="/"
-            exact={true}
-            render={({ location }) => (
-              <Redirect
-                to={{
-                  pathname: '/home',
-                  state: { from: location },
-                }}
-              />
-            )}
-          />
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <Router />
     </StoreProvider>
   );
 };
